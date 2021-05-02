c9e9c98db430b1c8cdbf164c62a69afc9c7413ea
ksrUniska <ksr.pmi.uniska0102@gmail.com>
2021-05-02 11:04:59 +0800

    initial commit

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg nav-orange">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		<div class="navbar-nav">
+			<a class="nav-item nav-link text-white navlink <?= $aktif == 'home' ? 'active' : '' ?>" href="index.php">HOME</a>
+			<a class="nav-item nav-link text-white navlink <?= $aktif == 'artikel' ? 'active' : '' ?>" href="artikel.php">KEGIATAN</a>
+			<a class="nav-item nav-link text-white navlink <?= $aktif == 'siswa' ? 'active' : '' ?>" href="siswa.php">Biodata Anggota</a>
+			<a class="nav-item nav-link text-white navlink <?= $aktif == 'guru' ? 'active' : '' ?>" href="guru.php">PEMBINA</a>
+			<a class="nav-item nav-link text-white navlink <?= $aktif == 'visi_misi' ? 'active' : '' ?>" href="visi_misi.php">PROFILE</a>
+			<a class="nav-item nav-link text-white navlink <?= $aktif == 'tentang_website' ? 'active' : '' ?>" href="tentang_website.php">TENTANG WEBSITE</a>
+		</div>
+	</div>
+</nav>
---
+++ visi_misi.php
@@ -0,0 +1,55 @@
+<?php 
+
+require_once 'koneksi.php';
+
+$query = mysqli_query($koneksi, "SELECT * FROM tbl_visi_misi WHERE id = 1");
+$visi_misi = mysqli_fetch_assoc($query);
+$aktif = 'visi_misi'; 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Detail Artikel - Anggota KSR PMI Unit UNISKA MAB</title>
+	<link rel="stylesheet" href="resources/fonts/stylesheet.css">
+	<link rel="stylesheet" href="resources/css/bootstrap.min.css">
+	<link rel="stylesheet" href="resources/css/style.css">
+</head>
+<body>
+	<div class="container bg-light">
+		<!-- top bar -->
+		<div class="logo clearfix">
+			<div class="float-left mt-3 mb-3">
+				<img src="resources/images/logoksr.png" alt="Logo Sekolah" width="70px" class="float-left mr-3">
+				<div class="text float-right">
+					<span class="smk"> KSR PMI Unit UNISKA MAB</span><br>
+					<span class="visi">Menyatu Dalam Rasa, Bersatu Untuk Sesama</span>
+				</div>
+			</div>
+		</div>
+			
+		<!-- nav bar -->
+		<?php require_once 'navbar.php'; ?>
+
+		<!-- content -->
+		<div class="row p-3">
+			<div class="col-md-8">
+				<div class="title mb-3">
+					Visi,Misi Dan Sejarah UKM KSR PMI Unit UNISKA MAB.
+				</div>
+				<div class="artikel">
+					<?= $visi_misi['visi_misi'] ?>
+				</div>
+			</div>
+			<?php require_once 'sidebar.php'; ?>
+		</div>
+		<div class="text-white footer">
+			2021 © Copyright by Anggota KSR PMI Unit UNISKA MAB.
+		</div>
+	</div>
+
+	<script src="resources/js/jquery.js"></script>
+	<script src="resources/js/bootstrap.min.js"></script>
+</body>
+</html>
